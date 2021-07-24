ec941f7573e5d67504b6c63f54444bab910f091f
Nora <noora.maleki@gmail.com>
2021-07-24 19:34:52 +0200

    initial commit

---
+++ 05_views.js
@@ -0,0 +1,188 @@
+// In this file you can find the instatiations of our views_seq
+//They are in order of their occurence in the experiment
+
+
+const intro = magpieViews.view_generator("intro",
+    //In the intro view we give a very short information about the experiment
+    //and about its assumed time scale
+    {
+      trials: 1,
+      name: 'intro',
+      text: `This is a follow up study concerning the answer to moral dilemmas and political stance.
+                <br />
+                <br />
+                On the following pages there you will have the choice between several political issues. Please select the one you that you are most interested in.
+                <br />
+                <br />
+                The experiment is going to take about 3 minutes.
+                <br />
+                <br />
+                By clicking the "begin the experiment" button you agree to participate in this experiment and that you have read the <a href="images/consent_form/consent_form.pdf" target="_blank">consent form</a>.`,
+      buttonText: 'begin the experiment'
+    }
+);
+
+
+const basic_information = basic_information_function(
+    //Here we display the pre-experiment background checked
+    //the function itself is defined in "03_custom_views_templates.js"
+    {
+        trials:1,
+        name: "basic_information",
+        title: "Personal information",
+        text: "Please fill in your personal information.",
+        button: "Next"
+    },
+    //answer_container_generator: answerContainerElem,
+)
+
+const choice_of_political_topic = magpieViews.view_generator('sentence_choice',
+    //In this view the participants are supposed to select the political topic
+    //they care about the most
+    {
+        trials: 1,
+        name: "choice_of_political_topic",
+        data: polit_choice,
+    },
+    {
+        //all the custom container functions can be found in "02_custom_functions.js"
+        stimulus_container_generator: show_only_title,
+        answer_container_generator: select_topic,
+        handle_response_function: select_statement
+    }
+
+);
+
+const rate_statement = rating(
+    //In this view the participants rate how much they agree/disagree with
+    //a given statement. The function is implemented in "03_custom_views_templates.js"
+    {
+        trials:1,
+        name: "rate_statement",
+        data: statements,
+        title: "Test title",
+    },
+)
+
+
+const instructions = magpieViews.view_generator("instructions",
+    //in this view we tell the participant that the study is a follow up and
+    //that it us about a moral dilemma
+    {
+        trials: 1,
+        name: 'instructions',
+        title: 'General Instructions',
+        text: `We are following up on a previously published paper that looked at how people feel about moral dilemmas.
+                <br />
+                <br />
+                In the previous paper, a moral dilemma was described that involved two possible courses of actions.
+                Participants chose which action they preferred and had to rate how they would feel about performing that action.
+                <br />
+                <br />
+                In this study, you will be presented with a scenario describing a moral dilemma.
+                You will choose which action you would take and then provide a rating of how good or bad you imagine you would feel after taking that action.`,
+        buttonText: 'Next'
+    }
+);
+
+const experimental_trial = magpieViews.view_generator("sentence_choice",
+    //In this view the participant is asked how they would respond to the
+    //moral dilemma. Additionally, the alleged response of their ingroup/outgroup
+    //is presented according to which group they are assigned to.
+    //For more information about the group assignement see the "assign_to_group"
+    //function in "02_custom_functions.js".
+    {
+        trials: 1,
+        title: "Experimental trial",
+        name: "experimental_trial",
+        data: moral_dilemma
+    },
+    {
+        stimulus_container_generator: assign_to_group,
+        answer_container_generator: select_response,
+        handle_response_function: response_experimental_trial
+    }
+)
+
+const fit_backstory_fake_rating = magpieViews.view_generator('rating_scale',
+    //In this view participants are asked to evalueate how good they feel about
+    //their decision in the previous view
+    {
+        trials: 1,
+        name: "fit_backstory_fake_rating",
+        data: fake_rating,
+      },
+      {
+          stimulus_container_generator: show_only_title
+      }
+);
+
+const understanding_check = magpieViews.view_generator('sentence_choice',
+    //In this view the participants are confronted with a question about how the
+    //alleged previous study was conducted. This is done in order to check then
+    //participants level of attention.
+    {
+        trials: 1,
+        name: "understanding_check",
+        data: understanding_questions,
+    },
+    {
+        stimulus_container_generator: show_only_title,
+        answer_container_generator: select_understanding_question,
+        handle_response_function: handle_response_functions.button_choice,
+    }
+
+);
+
+
+const identity_check = identity_check_function(
+    //Here the participants are asked to rate how much they identify with their
+    //"ingroup" (namely the people who have the same political opinion) and their
+    //"outgroup (the people who have opposite political opinions)"
+    {
+        trials: 1,
+        name: "identity_check",
+        title: "Identity Check",
+        data: identity_data,
+        button: "Next",
+    },
+);
+
+const understanding_check2  = magpieViews.view_generator('sentence_choice',
+    //In this view the participants are confronted with a question about how the
+    //alleged previous study was conducted. This is done in order to check then
+    //participants level of attention.
+    {
+        trials: 1,
+        name: "understanding_check2",
+        data: understanding_question2,
+    },
+    {
+        stimulus_container_generator: show_only_title,
+        answer_container_generator: select_understanding_question2,
+        handle_response_function: handle_response_functions.button_choice,
+    }
+
+);
+
+const sympathy_rating = magpieViews.view_generator('rating_scale',
+    //In this view participants are asked to evalueate how good they feel about
+    //their decision in the previous view
+    {
+        trials: 1,
+        name: "sympathy_rating",
+        data: sympathy_question,
+      },
+      {
+          stimulus_container_generator: sympathy_stimulus,
+          answer_container_generator: sympathy_answer
+      }
+);
+
+
+const thanks = magpieViews.view_generator("thanks", {
+  trials: 1,
+  name: 'thanks',
+  title: 'Thank you for taking part in this experiment!',
+  prolificConfirmText: 'Press the button'
+});
---
+++ 06_main.js
@@ -0,0 +1,65 @@
+// In this file you initialize and configure your experiment using magpieInit
+
+$("document").ready(function() {
+    // prevent scrolling when space is pressed
+    window.onkeydown = function(e) {
+        if (e.keyCode === 32 && e.target === document.body) {
+            e.preventDefault();
+        }
+    };
+    main.topic = "not_defined";
+    main.group = 0;
+    // calls magpieInit
+    // in debug mode this returns the magpie-object, which you can access in the console of your browser
+    // e.g. >> window.magpie_monitor or window.magpie_monitor.findNextView()
+    // in all other modes null will be returned
+    window.magpie_monitor = magpieInit({
+        // You have to specify all views you want to use in this experiment and the order of them
+        views_seq: [
+            //identity_check,
+            intro,
+            basic_information,
+            choice_of_political_topic,
+            rate_statement,
+            instructions,
+            experimental_trial,
+            fit_backstory_fake_rating,
+            understanding_check,
+            identity_check,
+            understanding_check2,
+            sympathy_rating,
+            thanks,
+        ],
+        // Here, you can specify all information for the deployment
+        deploy: {
+            experimentID: "258",
+            serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
+            // Possible deployment methods are:
+            // "debug" and "directLink"
+            // As well as "MTurk", "MTurkSandbox" and "Prolific"
+            deployMethod: "directLink",
+            contact_email: "YOUREMAIL@wherelifeisgreat.you",
+            prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
+        },
+        // Here, you can specify how the progress bar should look like
+        progress_bar: {
+            in: [
+              // intro.name,
+              // basic_information.name,
+              // choice_of_political_topic.name,
+              // rate_statement.name,
+              // instructions.name,
+              // experimental_trial.name,
+              // fit_backstory_fake_rating.name,
+              // understanding_check.name,
+              // identity_check.name,
+              // understanding_check2.name,
+              // sympathy_rating.name,
+              // thanks.name,
+            ],
+             // Possible styles are "default", "separate" and "chunks"
+            style: "default",
+            width: 150
+        }
+    });
+});
